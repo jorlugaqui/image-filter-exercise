2751f10ad33e3c65741f20f6068203a88a05b92c
Jorge Galvis <jorlugaqui@gmail.com>
2021-05-23 18:16:28 -0500

    feat: Add endpoint for supporting image filters
    As a local file has to be returned, it is important
    to delete it once the request finishes. That's why
    req.sendFile uses an fn(err), so we can remove it upon
    request completion.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+
 import express from 'express';
+import {Response, Request, NextFunction} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -12,10 +14,43 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+
+  app.get('/filteredimage', async (req: Request, res: Response, next: NextFunction) => {
+    // Validate the existence of the image_url query parameter
+    const imageURL: string = req.query.image_url;
+    if (!imageURL) {
+      return res.status(400).send({
+        message: 'An image URL is required: image_url={imageURL}'
+      });
+    }
+
+    let localImagePath: string = '';
+    try {
+      localImagePath = await filterImageFromURL(imageURL);
+    } catch (e) {
+      return res.status(500).send({
+        message: 'The URL provided could not be proccessed'
+      });
+    }
+
+    const options: object = {
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    };
+    return res.sendFile(localImagePath, options, function(error){
+      deleteLocalFiles([localImagePath]);
+      if (error) {
+        next(error);
+      }
+    });
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
